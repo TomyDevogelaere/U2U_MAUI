862a8edfca9afe84f70e97a7271f4a283f2159c0
Tomy Devogelaere <tomy.devogelaere@telenet.be>
2022-11-15 16:45:31 +0100

    Add customcontrols lab

---
+++ LabsMAUI/CustomControlLab/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomControlLab
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LabsMAUI/CustomControlLab/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomControlLab
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            IsBusy = true;
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            IsBusy = false;
+        }
+    }
+}
